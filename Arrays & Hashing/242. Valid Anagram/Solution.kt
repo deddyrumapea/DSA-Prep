2a238f17a07fd3280a00df7f1bf2664de8f42245
Deddy Romnan <romnandeddy@gmail.com>
2022-07-03 21:44:28 +0700

    solve 242. Valid Anagram

---
+++ Arrays & Hashing/242. Valid Anagram/Solution.kt
@@ -0,0 +1,34 @@
+// Time complexity: O(s + t + s) = O(n)
+// Space complexity: O(26) = O(1)
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+        val charCount = MutableList<Int>(26) { 0 }
+        for (char in s) charCount[char - 'a']++
+        for (char in t) charCount[char - 'a']--
+        for (count in charCount) if (count != 0) return false
+        return true
+    }
+}
+
+// Time complexity: O(s + s) = O(n)
+// Space complexity: O(s) = O(n)
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+
+        val countS = mutableMapOf<Char, Int>()
+        val countT = mutableMapOf<Char, Int>()
+
+        for (i in s.indices) {
+            countS[s[i]] = 1 + (countS[s[i]] ?: 0)
+            countT[t[i]] = 1 + (countT[t[i]] ?: 0)
+        }
+
+        for (entry in countS) {
+            if (countT[entry.key] != entry.value) return false
+        }
+
+        return true
+    }
+}
