2cf2a0d8b0f3af0491893341b0d02ba7499c06f5
Deddy Romnan <romnandeddy@gmail.com>
2022-07-04 12:31:09 +0700

    update binary search algorithm

--- Binary Search/704. Binary Search/Solution.kt
+++ Binary Search/704. Binary Search/Solution.kt
@@ -2,14 +2,14 @@ class Solution {
     fun search(nums: IntArray, target: Int): Int {
         var left = 0
         var right = nums.lastIndex
-
+        
         while (left <= right) {
-            val mid = (left + right) / 2
+            val mid = left + ((right - left) / 2)
             if (nums[mid] == target) return mid
-            if (nums[mid] < target) left = mid + 1
+            if (target > nums[mid]) left = mid + 1
             else right = mid - 1
         }
-
+        
         return -1
     }
 }
