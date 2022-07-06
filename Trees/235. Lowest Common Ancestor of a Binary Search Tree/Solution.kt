d3ef65713809d0417b0d4ec35989e19a64c27e5f
Deddy Romnan <romnandeddy@gmail.com>
2022-07-06 23:56:24 +0700

    solve 235. Lowest Common Ancestor of a Binary Search Tree

---
+++ Trees/235. Lowest Common Ancestor of a Binary Search Tree/Solution.kt
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int = 0) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+// Iterative solution
+// Time complexity: O(n)
+// Space complexity: O(1)
+class Solution {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {        
+        var ancestor = root
+        while(ancestor != null) {
+            when {
+                p == null -> return q
+                q == null -> return p
+                p.`val` < ancestor.`val` && q.`val` < ancestor.`val` -> ancestor = ancestor.left
+                p.`val` > ancestor.`val` && q.`val` > ancestor.`val` -> ancestor = ancestor.right
+                else -> return ancestor
+            }
+        }
+        return ancestor
+    }
+}
