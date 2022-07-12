088c8eb0e3497b49543f70a5f545011eef8f813c
Deddy Romnan <romnandeddy@gmail.com>
2022-07-12 22:21:55 +0700

    rewrite solution

--- Stack/20. Valid Parentheses/Solution.kt
+++ Stack/20. Valid Parentheses/Solution.kt
@@ -1,19 +1,16 @@
-import java.util.ArrayDeque
-
 class Solution {
     fun isValid(s: String): Boolean {
         val stack = ArrayDeque<Char>()
-
+        
         for (char in s) {
             when {
-                char == ')' && stack.peek() == '(' -> stack.remove()
-                char == '}' && stack.peek() == '{' -> stack.remove()
-                char == ']' && stack.peek() == '[' -> stack.remove()
-                setOf('(', '{', '[').contains(char) -> stack.push(char)
-                else -> return false
+                char == ')' && stack.peekLast() == '(' -> stack.removeLast()
+                char == '}' && stack.peekLast() == '{' -> stack.removeLast()
+                char == ']' && stack.peekLast() == '[' -> stack.removeLast()
+                else -> stack.addLast(char)
             }
         }
-
+        
         return stack.size == 0
     }
 }
