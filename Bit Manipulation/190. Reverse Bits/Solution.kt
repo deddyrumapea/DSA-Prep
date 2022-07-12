d15752affa5298b1a301722e1b5d21953416ce82
Deddy Romnan <romnandeddy@gmail.com>
2022-07-12 22:19:28 +0700

    rewrite solution

--- Bit Manipulation/190. Reverse Bits/Solution.kt
+++ Bit Manipulation/190. Reverse Bits/Solution.kt
@@ -1,13 +1,13 @@
 class Solution {
     // you need treat n as an unsigned value
-    fun reverseBits(n: Int): Int {
-        var res = 0
-
-        for (i in 0..32) {
-            val bit = (n shr i) and 1
-            res = res or (bit shl (31 - i))
+    fun reverseBits(n:Int): Int {
+        var result = 0
+        
+        for (i in 0..31) {
+            val bit = (n shr i) and 1 // Get the i-th bit using "and 1"
+            result = result or (bit shl (31 - i)) // Put the bit using "or" in the reversed order
         }
-
-        return res
+        
+        return result
     }
 }
