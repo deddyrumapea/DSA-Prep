ded119de8e8ee36dbf6bf613de6414d69e2f9308
Deddy Romnan <romnandeddy@gmail.com>
2022-07-25 17:19:10 +0700

    update solution

--- Sliding Window/3. Longest Substring Without Repeating Characters/Solution.kt
+++ Sliding Window/3. Longest Substring Without Repeating Characters/Solution.kt
@@ -1,21 +1,21 @@
 class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
-        val charMap = HashMap<Char, Int>()
+        val charSet = mutableSetOf<Char>()
         var left = 0
         var right = 0
-        var result = 0
-
-        while (left < s.length && right < s.length) {
-            val currentChar = s[right]
-            if (charMap.contains(currentChar)) {
-            	left = Math.max(left, charMap[currentChar]?.plus(1) ?: 0)
+        var maxLength = 0
+        
+        while (right <= s.lastIndex) {
+            while (charSet.contains(s[right])) {
+                charSet.remove(s[left])
+                ++left
             }
-
-            charMap[currentChar] = right
-            result = Math.max(result, right - left + 1)
-            right++
+            charSet.add(s[right])
+            val currLength = right - left + 1
+            maxLength = Math.max(maxLength, currLength)
+            ++right
         }
-
-        return result
+        
+        return maxLength
     }
 }
