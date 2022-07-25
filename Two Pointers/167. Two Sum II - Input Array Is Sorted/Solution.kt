76f41d8a2316f0b9d2e176394bda4552ba77f13f
Deddy Romnan <romnandeddy@gmail.com>
2022-07-25 15:25:02 +0700

    remove second solution

--- Two Pointers/167. Two Sum II - Input Array Is Sorted/Solution.kt
+++ Two Pointers/167. Two Sum II - Input Array Is Sorted/Solution.kt
@@ -3,27 +3,11 @@ class Solution {
         var left = 0
         var right = numbers.lastIndex
         
-        while((numbers[left] + numbers[right]) != target) {
-            if ((numbers[left] + numbers[right]) > target) right-- 
-            else left++
+        while ((numbers[left] + numbers[right]) != target) {
+            if ((numbers[left] + numbers[right]) < target) ++left
+            else --right
         }
         
         return intArrayOf(left + 1, right + 1)
-    }
-
-    fun twoSum2(numbers: IntArray, target: Int): IntArray {
-        var index1 = 0
-        var index2 = 0
-        
-        for (i in numbers.indices){
-            for (j in (i + 1)..numbers.lastIndex){
-                if ((numbers[i] + numbers[j]) == target){
-                    index1 = i + 1
-                    index2 = j + 1
-                }
-            }
-        }
-        
-        return intArrayOf(index1, index2)
     }
 }
