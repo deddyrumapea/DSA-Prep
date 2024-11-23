906340c7710d26ef75664908a0be2a907c83dfc9
Deddy Rumapea <romnandeddy@gmail.com>
2024-11-23 15:47:06 +0700

    Update Solution.kt

--- Stack/22. Generate Parentheses/Solution.kt
+++ Stack/22. Generate Parentheses/Solution.kt
@@ -1,25 +1,26 @@
 class Solution {
+
     private val stack = ArrayDeque<Char>()
     private val result = mutableListOf<String>()
-    
+
     fun generateParenthesis(n: Int): List<String> {
         backtrack(0, 0, n)
         return result
     }
-    
+
     private fun backtrack(openCount: Int, closedCount: Int, limitCount: Int) {
-        if (openCount == closedCount && closedCount == limitCount) {
+        if (openCount == limitCount && openCount == closedCount) {
             result.add(stack.joinToString(""))
             return
         }
-        
+
         if (openCount < limitCount) {
             stack.addLast('(')
             backtrack(openCount + 1, closedCount, limitCount)
             stack.removeLast()
         }
-        
-        if (closedCount < openCount) {
+
+        if (openCount > closedCount) {
             stack.addLast(')')
             backtrack(openCount, closedCount + 1, limitCount)
             stack.removeLast()
