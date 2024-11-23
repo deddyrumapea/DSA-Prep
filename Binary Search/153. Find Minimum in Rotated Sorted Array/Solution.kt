3fbbd134a4b7612918ebd412d6379842f3b1e21e
Deddy Rumapea <romnandeddy@gmail.com>
2024-11-23 17:06:37 +0700

    Update Solution.kt

--- Binary Search/153. Find Minimum in Rotated Sorted Array/Solution.kt
+++ Binary Search/153. Find Minimum in Rotated Sorted Array/Solution.kt
@@ -2,22 +2,24 @@ class Solution {
     fun findMin(nums: IntArray): Int {
         var left = 0
         var right = nums.lastIndex
-        
+
         var min = nums[0]
-        
+
         while (left <= right) {
             if (nums[left] < nums[right]) { // we're in a sorted sub array
                 min = Math.min(min, nums[left])
                 break
             }
-            
+
             val mid = left + (right - left) / 2
             min = Math.min(min, nums[mid])
-            
-            if (nums[mid] >= nums[left]) left = mid + 1
-            else right = mid - 1
+
+            when {
+                nums[mid] >= nums[left] -> left = mid + 1
+                else -> right = mid - 1
+            }
         }
-        
+
         return min
     }
 }
