b58de55e52931254abdc51f1c9c47150f8e06995
Deddy Rumapea <romnandeddy@gmail.com>
2024-11-23 20:38:46 +0700

    Update Solution.kt

--- Linked List/143. Reorder List/Solution.kt
+++ Linked List/143. Reorder List/Solution.kt
@@ -11,18 +11,19 @@ class Solution {
     fun reorderList(head: ListNode?): Unit {
         var slow = head
         var fast = head?.next
-        
+
+        // find middle node
         while (fast != null && fast.next != null) {
             slow = slow?.next
             fast = fast.next.next
         }
-        
         val mid = slow
-        
+
         var l1 = head
         var l2 = mid?.next
         mid?.next = null
-        
+
+        // reverse second portion of the linked list
         var prev: ListNode? = null
         while (l2 != null) {
             val next = l2.next
@@ -31,11 +32,12 @@ class Solution {
             l2 = next
         }
         l2 = prev
-        
+
+        // splice between first portion and reversed second portion
         while (l1 != null && l2 != null) {
             val next1 = l1.next
             val next2 = l2.next
-            
+
             l1.next = l2
             l2.next = next1
             l1 = next1
