d00585038d1f633e89d34f38efd79b254ba8048e
Deddy Rumapea <romnandeddy@gmail.com>
2024-11-23 17:12:18 +0700

    Update Solution.kt

--- Binary Search/33. Search in Rotated Sorted Array/Solution.kt
+++ Binary Search/33. Search in Rotated Sorted Array/Solution.kt
@@ -2,44 +2,29 @@ class Solution {
     fun search(nums: IntArray, target: Int): Int {
         var left = 0
         var right = nums.lastIndex
-        
-        while (left <= right) {
-            val mid = left + (right - left) / 2
-            
-            if (nums[mid] == target) return mid
-            
-            if (nums[left] <= nums[mid]) { // left sorted portion
-                if (target in nums[left]..nums[mid]) right = mid - 1
-                else left = mid + 1
-            } else { // right sorted portion
-                if (target in nums[mid]..nums[right]) left = mid + 1
-                else right = mid - 1
-            }
-        }
-        
-        return -1
-    }
-}
 
-class Solution {
-    fun search(nums: IntArray, target: Int): Int {
-        var left = 0
-        var right = nums.lastIndex
-        
         while (left <= right) {
             val mid = left + (right - left) / 2
-            
-            if (nums[mid] == target) return mid
-            
-            if (nums[left] <= nums[mid]) { // left sorted portion
-                if (target < nums[left] || target > nums[mid]) left = mid + 1
-                else right = mid - 1
-            } else { // right sorted portion
-                if (target > nums[right] || target < nums[mid]) right = mid - 1
-                else left = mid + 1
+
+            when {
+                nums[mid] == target -> return mid
+                
+                nums[left] <= nums[mid] -> { // left sorted portion
+                    when (target in nums[left]..nums[mid]) {
+                        true -> right = mid - 1
+                        false -> left = mid + 1
+                    }
+                }
+
+                else -> { // right sorted portion
+                    when (target in nums[mid]..nums[right]) {
+                        true -> left = mid + 1
+                        false -> right = mid - 1
+                    }
+                }
             }
         }
-        
+
         return -1
     }
 }
