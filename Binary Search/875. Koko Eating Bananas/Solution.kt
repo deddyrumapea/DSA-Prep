065c0677acafa8a88179e74355e68b66e793687d
Deddy Rumapea <romnandeddy@gmail.com>
2024-11-23 16:56:26 +0700

    Update Solution.kt

--- Binary Search/875. Koko Eating Bananas/Solution.kt
+++ Binary Search/875. Koko Eating Bananas/Solution.kt
@@ -1,27 +1,33 @@
 class Solution {
     fun minEatingSpeed(piles: IntArray, h: Int): Int {
         var maxPile = 0
-        for (pile in piles) maxPile = Math.max(maxPile, pile)
-        
+        for (pile in piles) {
+            maxPile = Math.max(maxPile, pile)
+        }
+
         var left = 1
         var right = maxPile
-        
+
         var minK = maxPile
-        
+
         while (left <= right) {
             val k = left + (right - left) / 2
-            
+
             var hours = 0.0
-            for (pile in piles) hours += Math.ceil(pile.toDouble() / k)
-            
-            if (hours <= h) {
-                minK = Math.min(minK, k)
-                right = k - 1
-            } else {
-                left = k + 1
+            for (pile in piles) {
+                hours += Math.ceil(pile.toDouble() / k)
+            }
+
+            when {
+                hours <= h -> {
+                    minK = Math.min(minK, k)
+                    right = k - 1
+                }
+
+                else -> left = k + 1
             }
         }
-        
+
         return minK
     }
-}   
+}
