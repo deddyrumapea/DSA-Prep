fe8e7ba40fd08d768e801d9be3b8d4545defcb55
Deddy Rumapea <romnandeddy@gmail.com>
2024-11-25 23:01:52 +0700

    Update Solution.kt

--- 1D Dynamic Programming/70. Climbing Stairs/Solution.kt
+++ 1D Dynamic Programming/70. Climbing Stairs/Solution.kt
@@ -2,13 +2,13 @@ class Solution {
     fun climbStairs(n: Int): Int {
         var currWays = 1
         var prevWays = 1
-        
-        for (i in (n - 2) downTo 0) {
+
+        repeat (n - 1) {
             val totalWays = currWays + prevWays
             currWays = prevWays
             prevWays = totalWays
         }
-        
+
         return prevWays
     }
 }
