c7072d9577a636ac9a20c4bcec3eadb53dbbebea
Deddy Rumapea <romnandeddy@gmail.com>
2024-11-26 19:26:53 +0700

    Update Solution.kt

--- Graphs/994. Rotting Oranges/Solution.kt
+++ Graphs/994. Rotting Oranges/Solution.kt
@@ -1,18 +1,18 @@
 class Solution {
     fun orangesRotting(grid: Array<IntArray>): Int {
         if (grid.isEmpty()) return 0
-        
+
         val queue = ArrayDeque<IntArray>()
-        
+
         var freshCount = 0
         var count = 0
 
-        // Put the position of all rotten oranges in queue
+        // put the possition of all rotten oranges in queue
         // count the number of fresh oranges
         for (row in grid.indices) {
             for (col in grid[row].indices) {
                 when (grid[row][col]) {
-                    1 -> freshCount++
+                    1 -> ++freshCount
                     2 -> queue.addLast(intArrayOf(row, col))
                 }
             }
@@ -24,7 +24,7 @@ class Solution {
             intArrayOf(1, 0),
             intArrayOf(-1, 0),
             intArrayOf(0, 1),
-            intArrayOf(0, -1)
+            intArrayOf(0, -1),
         )
 
         while (queue.isNotEmpty()) {
@@ -37,16 +37,19 @@ class Solution {
                     val x = point[0] + dir[0]
                     val y = point[1] + dir[1]
 
-                    if (!(x in grid.indices) || !(y in grid[0].indices) || grid[x][y] == 0 || grid[x][y] == 2) continue
+                    if (x !in grid.indices || y !in grid[x].indices || grid[x][y] == 0 || grid[x][y] == 2) continue
 
                     grid[x][y] = 2
                     queue.addLast(intArrayOf(x, y))
-                    
-                    freshCount--
+
+                    --freshCount
                 }
             }
         }
 
-        return if (freshCount == 0) count - 1 else -1
+        return when (freshCount == 0) {
+            true -> count - 1
+            false -> -1
+        }
     }
 }
