5108c053d697e1d8058b8028654135842d9cdc91
Deddy Rumapea <romnandeddy@gmail.com>
2024-11-26 15:52:48 +0700

    Update Solution.kt

--- Math & Geometry/202. Happy Number/Solution.kt
+++ Math & Geometry/202. Happy Number/Solution.kt
@@ -5,7 +5,7 @@ class Solution {
     fun isHappy(n: Int): Boolean {
         val checked = mutableSetOf<Int>()
         var num = n
-        
+
         while (!checked.contains(num)) {
             checked.add(num)
             
@@ -14,46 +14,15 @@ class Solution {
                 val intDigit = digit.toString().toInt()
                 sumOfSquares += (intDigit * intDigit)
             }
-            
-            if (sumOfSquares == 1) return true
-            else num = sumOfSquares
+
+            when (sumOfSquares == 1) {
+                true -> return true
+                false -> num = sumOfSquares
+            } 
         }
 
         return false
     }
-}
-
-// Time complexity: O(n)
-// Space complexity: O(n)
-class Solution {
-    fun isHappy(n: Int): Boolean {
-        val checked = mutableSetOf<Int>()
-        var num = n
-        
-        while (!checked.contains(num)) {
-            checked.add(num)
-            
-            val sumOfSquares = sumOfSquares(num) 
-            
-            if (sumOfSquares == 1) return true
-            else num = sumOfSquares
-        }
-
-        return false
-    }
-
-    private fun sumOfSquares(n: Int): Int {
-        var num = n
-        var sum = 0
-
-        while (num > 0) {
-            val digit = num % 10
-            sum += digit * digit
-            num = num / 10
-        }
-        
-        return sum
-    } 
 }
 
 // Time complexity: O(n)
@@ -72,14 +41,14 @@ class Solution {
         return true
     }
 
-    private fun sumOfSquares(num: Int): Int {
-        var mNum = num
+    private fun sumOfSquares(n: Int): Int {
+        var num = n
         var sum = 0
-        
-        while (mNum > 0) {
-            val digit = mNum % 10
+
+        while (num > 0) {
+            val digit = num % 10
             sum += digit * digit
-            mNum = mNum / 10
+            num = num / 10
         }
 
         return sum
