2b4aee1a17107fc93ad8e6a4db33e92922db5494
Deddy Rumapea <romnandeddy@gmail.com>
2024-11-26 20:01:20 +0700

    Update Solution.kt

--- Graphs/994. Rotting Oranges/Solution.kt
+++ Graphs/994. Rotting Oranges/Solution.kt
@@ -2,16 +2,16 @@ class Solution {
     fun orangesRotting(grid: Array<IntArray>): Int {
         if (grid.isEmpty()) return 0
 
-        val queue = ArrayDeque<IntArray>()
+        val infectors = ArrayDeque<IntArray>()
         var fresh = 0
 
-        // put the possition of all rotten oranges in queue
+        // put the possition of all rotten oranges in infectors
         // count the number of fresh oranges
         for (row in grid.indices) {
             for (col in grid[row].indices) {
                 when (grid[row][col]) {
                     1 -> ++fresh
-                    2 -> queue.addLast(intArrayOf(row, col))
+                    2 -> infectors.addLast(intArrayOf(row, col))
                 }
             }
         }
@@ -25,23 +25,23 @@ class Solution {
             intArrayOf(0, 1),
             intArrayOf(0, -1),
         )
-        while (queue.isNotEmpty()) {
-            repeat (queue.count()) {
-                val point = queue.removeFirst()
+        while (infectors.isNotEmpty()) {
+            repeat (infectors.count()) {
+                val infector = infectors.removeFirst()
 
                 for (dir in dirs) {
-                    val x = point[0] + dir[0]
-                    val y = point[1] + dir[1]
+                    val x = infector[0] + dir[0]
+                    val y = infector[1] + dir[1]
 
                     if (x !in grid.indices || y !in grid[x].indices || grid[x][y] == 0 || grid[x][y] == 2) continue
 
                     grid[x][y] = 2
-                    queue.addLast(intArrayOf(x, y))
+                    infectors.addLast(intArrayOf(x, y))
                     --fresh
                 }
             }
 
-            if (queue.isNotEmpty()) ++time
+            if (infectors.isNotEmpty()) ++time
         }
 
         return when (fresh == 0) {
