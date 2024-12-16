d3f091229daed3de28849540d5706ac4639b3ac5
Deddy Romnan <romnandeddy@gmail.com>
2024-12-16 11:40:52 +0700

    Update Solution.kt

--- Binary Search/4. Median of Two Sorted Arrays/Solution.kt
+++ Binary Search/4. Median of Two Sorted Arrays/Solution.kt
@@ -1,42 +1,38 @@
 class Solution {
     fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
-        val total = nums1.size + nums2.size
-        val half = (total + 1) / 2
-
         var a = nums1
         var b = nums2
-        
+
         if (b.size < a.size) {
             a = nums2
             b = nums1
         }
 
-        var l = 0
-        var r = a.size
+        val totalSize = nums1.size + nums2.size
+        val halfSize = (totalSize + 1) / 2
 
-        while (l <= r) {
-            val i = l + (r - l) / 2
-            val j = half - i
+        var aLeft = 0
+        var aRight = a.size
+        while (aLeft <= aRight) {
+            val aMid = aLeft + (aRight - aLeft) / 2
+            val bMid = halfSize - aMid
 
-            val aLeft = if (i > 0) a[i - 1] else Int.MIN_VALUE
-            val aRight = if (i < a.size) a[i] else Int.MAX_VALUE
-            val bLeft = if (j > 0) b[j - 1] else Int.MIN_VALUE
-            val bRight = if (j < b.size) b[j] else Int.MAX_VALUE
+            val aSmallMax = if (aMid > 0) a[aMid - 1] else Int.MIN_VALUE
+            val aBigMin = if (aMid < a.size) a[aMid] else Int.MAX_VALUE
+            val bSmallMax = if (bMid > 0) b[bMid - 1] else Int.MIN_VALUE
+            val bBigMin = if (bMid < b.size) b[bMid] else Int.MAX_VALUE
 
             when {
-                aLeft <= bRight && bLeft <= aRight -> {
-                    return when (total % 2 == 0) {
-                        true -> {
-                            (Math.max(aLeft.toDouble(), bLeft.toDouble()) +
-                                Math.min(aRight.toDouble(), bRight.toDouble())) / 2.0
-                        }
-                        false -> Math.max(aLeft.toDouble(), bLeft.toDouble())
+                aSmallMax <= bBigMin && bSmallMax <= aBigMin -> {
+                    return when (totalSize % 2 == 0) {
+                        true -> (Math.max(aSmallMax, bSmallMax) + Math.min(aBigMin, bBigMin)).toDouble() / 2
+                        false -> Math.max(aSmallMax, bSmallMax).toDouble()
                     }
                 }
 
-                aLeft > bRight -> r = i - 1
-
-                else -> l = i + 1
+                aSmallMax > bBigMin -> aRight = aMid - 1
+                
+                else -> aLeft = aMid + 1
             }
         }
 
